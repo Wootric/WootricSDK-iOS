66b818e2e41c1e0a898170717632f034a066a3c3
pJes2 <lukasz@copper.io>
2015-04-24 14:54:17 +0200

    Add test for WootricSDK

--- WootricSDK/WootricSDKObjCTests/WootricSDKTests.m
+++ WootricSDK/WootricSDKObjCTests/WootricSDKTests.m
@@ -8,8 +8,15 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "WootricSDK.h"
 
 @interface WootricSDKTests : XCTestCase
+
+@end
+
+@interface WootricSDK (Tests)
+
++ (void)setSurveyedInDefaults;
 
 @end
 
@@ -17,12 +24,17 @@
 
 - (void)setUp {
   [super setUp];
-  // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-  // Put teardown code here. This method is called after the invocation of each test method in the class.
   [super tearDown];
+  [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"surveyed"];
+}
+
+- (void)testsetSurveyedInDefaults {
+  [WootricSDK setSurveyedInDefaults];
+
+  XCTAssertTrue([[NSUserDefaults standardUserDefaults] boolForKey:@"surveyed"]);
 }
 
 @end
